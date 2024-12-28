25b56c341c7ed438be938741620f441252b09aa0
HDRJ-One <98429490+HDRJ-One@users.noreply.github.com>
2024-12-28 04:32:18 -0500

    fixes attempt

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,8 @@
 import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
-import { renderToReadableStream } from 'react-dom/server';
+import pkg from 'react-dom/server';
+const { renderToReadableStream } = pkg;
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
 import { themeStore } from '~/lib/stores/theme';
@@ -17,7 +18,11 @@ export default async function handleRequest(
     signal: request.signal,
     onError(error: unknown) {
       console.error(error);
+      responseHeaders.set('Content-Type', 'text/plain');
+      responseHeaders.set('X-Error-Message', 'Internal Server Error');
       responseStatusCode = 500;
+      responseHeaders.set('Cache-Control', 'no-cache');
+      responseHeaders.set('Expires', '0');
     },
   });
 
@@ -66,10 +71,11 @@ export default async function handleRequest(
     await readable.allReady;
   }
 
-  responseHeaders.set('Content-Type', 'text/html');
-
-  responseHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
-  responseHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+  if (responseStatusCode !== 500) {
+    responseHeaders.set('Content-Type', 'text/html');
+    responseHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
+    responseHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+  }
 
   return new Response(body, {
     headers: responseHeaders,
